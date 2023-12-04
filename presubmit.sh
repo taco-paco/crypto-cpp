7d8ae57f9641e5bbb90f595278f964a784f1e6ad
edwin <edwinswatpako@gmail.com>
2023-12-04 14:00:41 +0700

    Modified presubmit

--- presubmit.sh
+++ presubmit.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install npm packages.
-(cd src/starkware/crypto/ffi/js; npm install)
+(cd libs/crypto/ffi/js; npm install)
 
 # Compile the code.
 mkdir -p build/Release
@@ -14,7 +14,7 @@ make -C build/Release
 CTEST_OUTPUT_ON_FAILURE=1 make -C build/Release test
 
 # Run go testing (we assume golang-go is installed).
-go test build/Release/src/starkware/crypto/ffi/crypto_lib_test.go
+go test build/Release/libs/crypto/ffi/crypto_lib_test.go
 
-clang-tidy-6.0 -header-filter=src/starkware -p=build/Release $(find src/starkware -name "*.cc" | grep -v node_modules)
-cpplint --extensions=cc,h $(find src/starkware | grep -v node_modules | grep -E '\.(cc|h)$')
+clang-tidy-6.0 -header-filter=libs -p=build/Release $(find libs -name "*.cc" | grep -v node_modules)
+cpplint --extensions=cc,h $(find libs | grep -v node_modules | grep -E '\.(cc|h)$')
